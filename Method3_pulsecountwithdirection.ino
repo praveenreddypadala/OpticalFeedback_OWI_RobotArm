6fcb0cf43ee3d257e684312ae719d395fc0dbabe
Praveen Kumar Reddy Padala <32118883+praveenreddypadala@users.noreply.github.com>
2017-12-13 07:40:40 -0600

    Update Method3_pulsecountwithdirection.ino

--- Method3_pulsecountwithdirection.ino
+++ Method3_pulsecountwithdirection.ino
@@ -1,14 +1,16 @@
 /* Inthis method a single IR line detecting Module and a little circuitry to detect the direction of rotation are 
 combined to either increment or decrement the pulse count which gives the position of the Robotic Arm*/
-const int in_pin =2;
+
 volatile long ticks = 0;
 volatile int val1=0;
 void setup()
 {
   Serial.begin(9600);
-  pinMode(in_pin,INPUT);
-  attachInterrupt(0,countTicks,RISING);
+  pinMode(2,INPUT);//Digital Pin2 is defined as Input
+  attachInterrupt(0,countTicks,RISING); //Here number Zero means Interrupt INT0 is seen at Digital Pin2 
+  //All the pulses will be seen as interrupts so as to not miss any pulses
 }
+
 void loop()
 {
   Serial.println(ticks);//Gives current position
@@ -19,11 +21,11 @@ void countTicks()
   val1=analogRead(A0);
   if (val1>100)
   {
-  ticks++;
+  ticks++;//Motor is Moving forward,Increment Count
   }
   else
   {
-    ticks--;
+    ticks--;//Motor is Moving backward,Decrement Count
   }
 }
   
